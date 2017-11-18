7668f137ff0c30726462521c345db318f2629d40
Luis Nunez <luis.nunez@ucsp.edu.pe>
2017-11-18 15:06:00 -0500

    Mapeo de Curso

--- src/main/java/domain/Curso.java
+++ src/main/java/domain/Curso.java
@@ -2,15 +2,34 @@ package domain;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Curso implements BaseEntity<Long> {
+	@Id
 	private Long id;
 
+	@Column(length=19, unique=true)
 	private String codigo;
 
+	@Column(length=128)
 	private String nombre;
 
+	@Column
 	private Integer creditos;
 
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name="prerrequisito",
+			joinColumns = @JoinColumn(name="curso_codigo",referencedColumnName="codigo"),
+			inverseJoinColumns = @JoinColumn(name="prerrequisito_codigo",
+			referencedColumnName = "codigo"))
 	private List<Curso> prerequisitos;
 
 	@Override
